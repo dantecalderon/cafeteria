bb4fa6865a4b1b3bc9ff200e17c1ce082d43d4ba
Dante Calderón <dantehemerson@gmail.com>
2018-05-08 22:13:57 -0500

    Sweet alert añadido

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,10 +3,16 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
+import swal from 'sweetalert2'
+
+swal('Hola1!!');
+
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+
+
 
 
 $(document).foundation();
